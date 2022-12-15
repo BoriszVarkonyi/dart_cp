1909d8f4c01bb9377db6db75811f866a6eaa2545
KristofWolfram <wolfram.kristof@gmail.com>
2022-12-15 22:04:38 +0100

    Merged frontend and backend folders

--- frontend/src/components/layouts/ControlLayout/ControlLayout.js
+++ frontend/src/components/layouts/ControlLayout/ControlLayout.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./ControlLayout.styles.css";
 import { Route, Routes } from "react-router-dom";
 import { ControlRoutes } from "./ControlRoutes";
-import NavBar from "../../../components/static/NavBar";
-import Header from "../../../components/static/Header";
+//import NavBar from "../../../components/static/NavBar";
+//import Header from "../../../components/static/Header";
 
 
 export default function ControlLayout() {
     return (
 
         <div className="ControlLayout">
-            <Header />
-            <NavBar />
+            {/*
+                <Header />
+                <NavBar />
+            */}
             <Routes>
                 <Route path=":id/*" element={<ControlRoutes />} />
             </Routes>
