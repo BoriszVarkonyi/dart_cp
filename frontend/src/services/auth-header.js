435f677cb1cf6cfecb8deb34780542f87f51cb0a
tothzalan <toth.zalan03@gmail.com>
2022-12-19 20:28:34 +0100

    Basic general service backend

--- frontend/src/services/auth-header.js
+++ frontend/src/services/auth-header.js
@@ -1,8 +1,7 @@
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
-
-  if (user && user.accessToken) {
-    return { Authorization: 'Bearer ' + user.accessToken };
+  if (user && user.access) {
+    return `Bearer ${user.access}`;
   } else {
     return {};
   }
---
+++ frontend/src/services/backend.service.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8082/api/',
+  timeout: 1000,
+  headers: { 'Authorization': authHeader() }
+});
+
+const config = {
+  headers: authHeader(),
+};
+
+async function get (url) {
+  const resp = await instance.get(`${url}`, {
+    config
+  });
+  return await resp.data;
+};
+
+async function post (url, payload) {
+  try {
+    const resp = await instance.post(`${url}`, {
+      ...payload,
+      config
+    });
+    return resp.data;
+  } catch (err) {
+    return err;
+  }
+}
+
+export {
+  get,
+  post
+}
