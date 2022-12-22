73fbcb54b6fb8438fdb14f7e48b1557870d473f0
tothzalan <toth.zalan03@gmail.com>
2022-12-22 20:21:11 +0100

    Tournament has_comp action eg. /api/tournaments/1/hascomp/1/

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import * 
 from .serializers import *
 
@@ -16,6 +19,13 @@ class TournamentViewSet(viewsets.ModelViewSet):
   queryset = TournamentModel.objects.all()
   serializer_class = TournamentSerializer
   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+  @action(detail=True, url_path='hascomp/(?P<comp_pk>[0-9]+)')
+  def has_comp(self, request, comp_pk, pk=None):
+    get_object_or_404(CompetitionModel,
+                      pk=comp_pk,
+                      assoc_tournament_id=self.get_object())
+    return Response({'detail': 'Found.'}, status=200)
 
 class WeaponControlViewSet(viewsets.ModelViewSet):
   queryset = WeaponControlModel.objects.all()
