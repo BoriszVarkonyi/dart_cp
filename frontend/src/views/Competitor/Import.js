0313e699fe4a5873357b65b185efea70c86b9ee7
KristofWolfram <wolfram.kristof@gmail.com>
2023-01-03 21:50:51 +0100

    Visual improvements and more consistent design theme

--- frontend/src/views/Competitor/Import.js
+++ frontend/src/views/Competitor/Import.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export default function Import(){
-    return(
-        <div className="Panel">
-            <h1>Ez itt a Import oldal</h1>
+export default function Import() {
+    return (
+        <div className="Main">
+            <div className="PageHeader">
+                <h2 className="PageTitle">Import XML</h2>
+                <div className="PageButtonsWrapper">
+                    <Button variant="contained" size="small">Cancel</Button>
+                    <Button variant="contained" size="small">Add // Save Changes</Button>
+                </div>
+            </div>
+            <div className="PanelContentSingle">
+                <div className="TableGrid">
+
+                </div>
+            </div>
         </div>
     )
 }
