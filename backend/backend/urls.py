6053fb352f34d9a522eabd3773e7632dc46f9d36
KovacsM <kmarcell.kovi@gmail.com>
2023-01-12 19:45:31 +0100

    not yet finished move files from latop to pc

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,12 +7,22 @@ router.register(r'fencers', views.FencerViewSet)
 router.register(r'competitions', views.CompetitionViewSet)
 router.register(r'tournaments', views.TournamentViewSet)
 router.register(r'weaponcontrols', views.WeaponControlViewSet)
+router.register(r'registrations', views.RegistrationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('uploadxml/', views.MyUploadView.as_view()),
-    path('tournaments/<int:tournament>/competitions/', views.TournamentCompetitionsView.as_view()),
-    path('competitions/<int:competition>/fencers/', views.CompetitionsFencerView.as_view()),
+    path(
+        'uploadxml/',
+        views.MyUploadView.as_view()
+    ),
+    path(
+        'tournaments/<int:tournament>/competitions/',
+        views.TournamentCompetitionsView.as_view()
+    ),
+    path(
+        'competitions/<int:competition>/fencers/',
+        views.CompetitionsFencerView.as_view()
+    ),
     path(
         'stats/fencers/<str:fencer>/',
         views.FencersCompetitionsView.as_view()
@@ -24,5 +34,13 @@ urlpatterns = [
     path(
         'stats/weaponcontrols/issues/<str:competition>/<str:fencer>/',
         views.WeaponControlFencersIssues.as_view()
+    ),
+    path(
+        'in-register/<str:competition>/<str:fencer>/',
+        views.RegisterFencerIn.as_view(),
+    ),
+    path(
+        'un-register/<str:competition>/<str:fencer>/',
+        views.RegisterFencerOut.as_view(),
     ),
 ]
