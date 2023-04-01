c7d42cfd807c7b4e0fb40d510e5523e3602fc1ac
tothzalan <toth.zalan03@gmail.com>
2023-04-01 22:57:32 +0200

    Login i18n

--- frontend/src/components/layouts/LoginLayout/LoginLayout.js
+++ frontend/src/components/layouts/LoginLayout/LoginLayout.js
@@ -1,23 +1,43 @@
-import React from "react";
-import "./LoginLayout.css";
-import fencersImage from "../../../assets/fencers.svg";
-import horseImage from "../../../assets/horse.svg";
-import Login from "../../../views/Login/Login";
+import React from 'react';
+import './LoginLayout.css';
+import fencersImage from '../../../assets/fencers.svg';
+import horseImage from '../../../assets/horse.svg';
+import Login from '../../../views/Login/Login';
+import Button from '@mui/material/Button';
+import { useTranslation, Trans } from 'react-i18next';
 
 export default function LoginLayout() {
-    return (
-        <div className="LoginLayout">
-            <div className="LoginBlock">
-                <div className="LoginHeader">
-                    <img className="LogoImage" src={horseImage} />
-                    <h1 className="LoginText">Login</h1>
-                </div>
-                <Login />
-                <p className="CopyrightText">© d'ARTAGNAN 2022</p>
-            </div>
-            <div className="LoginBackground">
-                <img className="FencersImage" src={fencersImage} />
-            </div>
+  const { t, i18n } = useTranslation();
+  return (
+    <div className="LoginLayout">
+      <div className="LoginBlock">
+        <div className="LoginHeader">
+          <img className="LogoImage" src={horseImage} />
+          <h1 className="LoginText">{t('login.login')}</h1>
         </div>
-    );
+        <Login />
+        <div className="LanguageSwitcher">
+          <Button
+            variant="text"
+            size="large"
+            onClick={() => i18n.changeLanguage('en')}
+          >
+            🇬🇧
+          </Button>
+          &#47;
+          <Button
+            variant="text"
+            size="large"
+            onClick={() => i18n.changeLanguage('hu')}
+          >
+            🇭🇺
+          </Button>
+        </div>
+        <p className="CopyrightText">© d'ARTAGNAN 2023</p>
+      </div>
+      <div className="LoginBackground">
+        <img className="FencersImage" src={fencersImage} />
+      </div>
+    </div>
+  );
 }
---
+++ frontend/src/i18n.js
@@ -0,0 +1,46 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+//import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  // .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: {
+          login: {
+            email: 'Email',
+            password: 'Password',
+            login: 'Login',
+            enterEmail: 'Please enter your email address!',
+            enterValidEmail: 'Please enter a valid email address!',
+            enterPassword: 'Please enter your password!',
+          },
+        },
+      },
+      hu: {
+        translation: {
+          login: {
+            email: 'E-mail',
+            password: 'Jelszó',
+            login: 'Bejelentkezés',
+            enterEmail: 'Kérjük adja meg az e-mail-címét!',
+            enterValidEmail: 'Kérjük érvényes e-mail-címet adjon meg!',
+            enterPassword: 'Kérjük adja meg a jelszavát!',
+          },
+        },
+      },
+    },
+  });
+
+export default i18n;
