6df341cb55da7c6c67257f6d361f5ca3a919a1b8
KristofWolfram <wolfram.kristof@gmail.com>
2023-04-10 16:43:59 +0200

    WCStats removed react-pdf lib as it was sufficient way of handling PDF generation

--- frontend/src/views/WeaponControl/WeaponControlStatistics.js
+++ frontend/src/views/WeaponControl/WeaponControlStatistics.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import "./WeaponControlStatistics.css";
-
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
-import { PDFDownloadLink } from "@react-pdf/renderer";
 import { DataGrid } from "@mui/x-data-grid";
 import { ResponsivePieCanvas } from '@nivo/pie'
 
@@ -232,47 +229,18 @@ export default function WeaponControlStatistics() {
     />
   )
 
-  // Statistics document styling
-  const DocumentStyles = StyleSheet.create({
-    page: {
-      flexDirection: 'row',
-      backgroundColor: '#E4E4E4'
-    },
-    section: {
-      margin: 10,
-      padding: 10,
-      flexGrow: 1
-    }
-  });
-
-  // Statistics document declaration
-  const WCStatisticsDocument = () => (
-    <Document>
-      <Page size="A4" style={DocumentStyles.page}>
-        <View style={DocumentStyles.section}>
-          <Text>Section #1</Text>
-        </View>
-        <View style={DocumentStyles.section}>
-          <Text>Section #2</Text>
-        </View>
-      </Page>
-    </Document>
-  );
-
   return (
     <div className="Main">
       <div className="PageHeader">
         <h2 className="PageTitle">Weapon Control Statistics</h2>
         <div className="PageButtonsWrapper">
-          {/* Download Data */}
-          <PDFDownloadLink document={<WCStatisticsDocument />} fileName="_compname_weapon_control_statistics">
-            <Button variant="contained" size="small">
-              DOWNLOAD PDF
-            </Button>
-          </PDFDownloadLink>
+          <Button variant="contained" size="small">
+            DOWNLOAD PDF
+          </Button>
         </div>
       </div>
       <div className="ColumnPage">
+        temporary, just testing
         <MyResponsivePieCanvas />
         <p className="PageSectionTitle">SUMMARY</p>
         <div className="PageSection">
