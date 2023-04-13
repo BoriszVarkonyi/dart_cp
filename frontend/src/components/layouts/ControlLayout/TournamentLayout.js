74adfb8883d4d6ee29d14bbe231af2659794f8c9
KristofWolfram <wolfram.kristof@gmail.com>
2023-04-13 23:14:02 +0200

    Loading Screen small changes made for presentation

--- frontend/src/components/layouts/ControlLayout/TournamentLayout.js
+++ frontend/src/components/layouts/ControlLayout/TournamentLayout.js
@@ -3,7 +3,6 @@ import NavBar from "../../static/NavBar/NavBar";
 import Header from "../../static/Header/Header";
 import { Outlet } from "react-router-dom";
 import "./TournamentLayout.css";
-import Loading from "../../static/Loading/Loading";
 
 export function TournamentLayout() {
   return (
