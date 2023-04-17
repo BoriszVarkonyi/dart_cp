c06ff29da55f2d881bc242556c5df30d9e075e76
MEEP0321 <polyak.attila56@gmail.com>
2023-04-17 21:37:37 +0200

    tour load

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,18 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { CookiesProvider } from "react-cookie";
 import store from "./store";
 import "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+    <CookiesProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </CookiesProvider>
 );
