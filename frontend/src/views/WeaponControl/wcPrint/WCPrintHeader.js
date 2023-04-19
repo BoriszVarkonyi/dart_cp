6fdd171396eb25fc4f5e2265a9ae2fa486acb35d
tothzalan <toth.zalan03@gmail.com>
2023-04-19 11:59:39 +0200

    Fix errors For the love of God, please don't push not working code

--- frontend/src/views/WeaponControl/wcPrint/WCPrintHeader.js
+++ frontend/src/views/WeaponControl/wcPrint/WCPrintHeader.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { get } from "../../services/backend.service";
-import { useState } from "react";
+import { get } from '../../../services/backend.service';
+import { useState, useEffect } from "react";
 
 export default function WCPrintHeader(props) {
+  const compId = props.compId;
+  const tournamentId = props.tournamentId;
   const [currentComp, setCurrentComp] = useState();
   const [currentTour, setCurrentTour] = useState();
   async function getData() {
