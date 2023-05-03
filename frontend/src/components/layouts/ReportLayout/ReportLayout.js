52c9305d331171dc662361593eda1168fac1430e
MEEP0321 <polyak.attila56@gmail.com>
2023-05-03 23:17:29 +0200

    Wc reports. D-O-N-E

---
+++ frontend/src/components/layouts/ReportLayout/ReportLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import WCLiveReports from "../../../views/WeaponControl/Reports/WCLiveReports";
+
+export default function ReportLayout(){
+    return(
+        <main>
+            <p>hello</p>
+            <div className="PageContent">
+            <WCLiveReports/>
+            </div>
+        </main>
+    )
+}
