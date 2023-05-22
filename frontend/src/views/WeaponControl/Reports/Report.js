fb51ee3cf882362e312e6b6c230ecedfe5bd0d44
MEEP0321 <polyak.attila56@gmail.com>
2023-05-22 19:51:53 +0200

    was, were.....

--- frontend/src/views/WeaponControl/Reports/Report.js
+++ frontend/src/views/WeaponControl/Reports/Report.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Report(props) {
     const reportProps = props.props
+
     return (
         <div className="ReportCard">
             <div className="CardName">
@@ -14,7 +15,7 @@ export default function Report(props) {
             </div>
             <div className="CardIssues">
                 <div>
-                    {reportProps.fIssues.length > 0? (reportProps.fIssues.map((e) => <p key={e.issueName}>{e.issueName} <span>{e.issueNum}</span></p>)) : (<p>There was no issues</p>) }
+                    {reportProps.fIssues.length > 0? (reportProps.fIssues.map((e) => <p key={e.issueName}>{e.issueName} <span>{e.issueNum}</span></p>)) : (<p>There were no issues</p>) }
                 </div>
             </div>
         </div>
